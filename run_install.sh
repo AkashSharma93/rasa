7209fece136686695b2d4d655a9abf302d513be7
Akash Sharma S <akash@zapr.in>
2021-03-26 05:05:23 +0530

    Update install script.

--- run_install.sh
+++ run_install.sh
@@ -1,9 +1,6 @@
 echo "Starting custom installation."
-pip uninstall -y hydra-core omegaconf
-pip install torch poetry widgetsnbextension ipywidgets
+pip install torch poetry
 make install
 pip uninstall -y typing
-pip install tensorflow_text==2.1.1
-pip install hydra-core omegaconf
 pip install transformers==3.0.0
 echo "Completed custom installation."
