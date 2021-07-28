4d78a1da0039f471cc8d371bd4ff83288be8d307
Tobias Wochinger <t.wochinger@rasa.com>
2021-07-28 16:09:44 +0200

    implement initial caching logic

--- rasa/engine/caching.py
+++ rasa/engine/caching.py
@@ -1,2 +1,236 @@
+import logging
+import os
+import uuid
+from datetime import datetime
+from pathlib import Path
+from typing import Text, Any, Optional, Tuple, Type
+
+from sqlalchemy.engine import URL
+
+# TODO: Make this a prod dependency
+from typing_extensions import Protocol, runtime_checkable
+
+import rasa
+import rasa.shared.utils.common
+from rasa.engine.model_storage import ModelStorage
+import sqlalchemy as sa
+import sqlalchemy.orm
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CACHE_LOCATION = Path(".rasa", "cache")
+DEFAULT_CACHE_NAME = "cache.db"
+
+CACHE_LOCATION_ENV = "RASA_CACHE_DIRECTORY"
+CACHE_DB_NAME_ENV = "RASA_CACHE_NAME"
+
+
+@runtime_checkable
+class Cacheable(Protocol):
+    """Protocol for cacheable graph component outputs.
+
+    We only cache graph component outputs which are `Cacheable`. We only store the
+    output fingerprint for everything else.
+    """
+
+    def to_cache(self, directory: Path, model_storage: ModelStorage) -> None:
+        """Persists `Cacheable` to disk.
+
+        Args:
+            directory: The directory where the `Cacheable` can persist itself to.
+            model_storage: The current model storage (e.g. used when caching `Resource`
+                objects.
+        """
+        ...
+
+    @classmethod
+    def from_cache(
+        cls, node_name: Text, directory: Path, model_storage: ModelStorage
+    ) -> "Cacheable":
+        """Loads `Cacheable` from cache.
+
+        Args:
+            node_name: The name of the graph node which wants to use this cached result.
+            directory: Directory containing the persisted `Cacheable`.
+            model_storage: The current model storage (e.g. used when restoring
+                `Resource` objects so that they can fill the model storage with data.
+
+        Returns:
+            Instantiated `Cacheable`.
+        """
+        ...
+
+
 class TrainingCache:
-    pass
+    """Stores training results in a persistent cache.
+
+    Used to minimize re-retraining when the data / config didn't change in between
+    training runs.
+    """
+
+    from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+
+    Base: DeclarativeMeta = declarative_base()
+
+    class CacheEntry(Base):
+        """Stores metadata about a single cache entry."""
+
+        __tablename__ = "cache_entry"
+
+        # `create_sequence` is needed to create a sequence for databases that
+        # don't autoincrement Integer primary keys (e.g. Oracle)
+        fingerprint_key = sa.Column(sa.String(), primary_key=True)
+        output_fingerprint_key = sa.Column(sa.String(), nullable=False, index=True)
+        last_used = sa.Column(sa.DateTime(), nullable=False)
+        rasa_version = sa.Column(sa.String(255), nullable=False)
+        result_location = sa.Column(sa.String())
+        result_type = sa.Column(sa.String())
+
+    def __init__(self) -> None:
+        """Creates cache.
+
+        The `Cache` setting can be configured via environment variables.
+        """
+        self._cache_location = Path(
+            os.environ.get(CACHE_LOCATION_ENV, DEFAULT_CACHE_LOCATION)
+        )
+
+        if not self._cache_location.exists():
+            self._cache_location.mkdir(parents=True)
+
+        self._cache_database_name = os.environ.get(
+            CACHE_DB_NAME_ENV, DEFAULT_CACHE_NAME
+        )
+
+        engine = sa.create_engine(
+            URL.create(
+                drivername="sqlite",
+                database=str(self._cache_location / self._cache_database_name),
+            )
+        )
+        self.Base.metadata.create_all(engine)
+        self._sessionmaker = sa.orm.sessionmaker(engine)
+
+    def cache_output(
+        self,
+        fingerprint_key: Text,
+        output: Any,
+        output_fingerprint: Text,
+        model_storage: ModelStorage,
+    ) -> None:
+        """Adds the output to the cache.
+
+        If the output is of type `Cacheable` the output is persisted to disk in addition
+        to its fingerprint.
+
+        Args:
+            fingerprint_key: The fingerprint key serves as key for the cache. Graph
+                components can use their fingerprint key to lookup fingerprints of
+                previous training runs.
+            output: The output. The output is only cached to disk if it's of type
+                `Cacheable`.
+            output_fingerprint: The fingerprint of they output. This can be used
+                to lookup potentially persisted outputs on disk.
+            model_storage: Required for caching `Cacheable` instances. E.g. `Resource`s
+                use that to copy data from the model storage to the cache.
+        """
+        cache_dir, output_type = None, None
+        if isinstance(output, Cacheable):
+            cache_dir, output_type = self._cache_output_to_disk(output, model_storage)
+
+        with self._sessionmaker.begin() as session:
+            cache_entry = self.CacheEntry(
+                fingerprint_key=fingerprint_key,
+                output_fingerprint_key=output_fingerprint,
+                last_used=datetime.utcnow(),
+                rasa_version=rasa.__version__,
+                result_location=cache_dir,
+                result_type=output_type,
+            )
+            session.add(cache_entry)
+
+    def _cache_output_to_disk(
+        self, output: Cacheable, model_storage: ModelStorage
+    ) -> Tuple[Optional[Text], Optional[Text]]:
+        cache_dir = self._cache_location / uuid.uuid4().hex
+        cache_dir.mkdir()
+        try:
+            output.to_cache(cache_dir, model_storage)
+            output_type = rasa.shared.utils.common.module_path_from_instance(output)
+
+            logger.debug(f"Caching output of type '{type(output)}' succeeded.")
+            return str(cache_dir.absolute()), output_type
+        except Exception as e:
+            logger.error(
+                f"Caching output of type '{type(output)}' failed with the "
+                f"following error:\n{e}"
+            )
+            return None, None
+
+    def get_cached_output_fingerprint(self, fingerprint_key: Text) -> Optional[Text]:
+        """Retrieves fingerprint of output based on fingerprint key.
+
+        Args:
+            fingerprint_key: The fingerprint serves as key for the lookup of output
+                fingerprints.
+
+        Returns:
+            The fingerprint of a matching output or `None` in case no cache entry was
+            found for the given fingerprint key.
+        """
+        with self._sessionmaker.begin() as session:
+            query = sa.select(self.CacheEntry.output_fingerprint_key).filter_by(
+                fingerprint_key=fingerprint_key
+            )
+            match = session.execute(query).first()
+
+            if match:
+                return match.output_fingerprint_key
+
+            return None
+
+    def get_cached_result(
+        self, output_fingerprint_key: Text
+    ) -> Tuple[Optional[Path], Optional[Type[Cacheable]]]:
+        """Returns a potentially cached output result.
+
+        Args:
+            output_fingerprint_key:
+
+        Returns:
+            - Path to directory containing the cached content or `None` in case no
+            persisted data was found
+            - Class of persisted data or None in case no persisted data was found.
+        """
+        with self._sessionmaker.begin() as session:
+            query = sa.select(
+                self.CacheEntry.result_location, self.CacheEntry.result_type
+            ).where(
+                self.CacheEntry.output_fingerprint_key == output_fingerprint_key,
+                self.CacheEntry.result_location != sa.null(),
+            )
+            match = session.execute(query).first()
+
+        if not match:
+            logger.debug(f"No cached output found for '{output_fingerprint_key}'")
+            return None, None
+
+        path_to_cached = Path(match.result_location)
+        if not path_to_cached.is_dir():
+            logger.debug(
+                f"Cached output for '{output_fingerprint_key}' can't be found on disk."
+            )
+            return None, None
+
+        module_as_string = match.result_type
+        try:
+            module = rasa.shared.utils.common.class_from_module_path(module_as_string)
+            assert isinstance(module, Cacheable)
+
+            return path_to_cached, module
+        except Exception as e:
+            logger.warning(
+                f"Failed to find module for cached output of type "
+                f"'{module_as_string}'. Error:\n{e}"
+            )
+            return None, None
--- tests/engine/test_caching.py
+++ tests/engine/test_caching.py
@@ -1,10 +1,148 @@
-from rasa.engine.caching import TrainingCache
+import dataclasses
+import logging
+import shutil
+import uuid
+from pathlib import Path
+from typing import Dict, Text
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from _pytest.monkeypatch import MonkeyPatch
+
+import rasa.shared.utils.io
+from rasa.engine.caching import TrainingCache, CACHE_LOCATION_ENV
+from rasa.engine.model_storage import ModelStorage
 
 
-def test_cache_output():
+@pytest.fixture()
+def temp_cache(tmp_path: Path, monkeypatch: MonkeyPatch) -> TrainingCache:
+    monkeypatch.setenv(CACHE_LOCATION_ENV, str(tmp_path))
+    return TrainingCache()
+
+
+@dataclasses.dataclass
+class TestCacheableOutput:
+
+    value: Dict
+
+    def to_cache(self, directory: Path, model_storage: ModelStorage) -> None:
+        rasa.shared.utils.io.dump_obj_as_json_to_file(
+            directory / "cached.json", self.value
+        )
+
+    @classmethod
+    def from_cache(
+        cls, node_name: Text, directory: Path, model_storage: ModelStorage
+    ) -> "TestCacheableOutput":
+        value = rasa.shared.utils.io.read_json_file(directory / "cached.json")
+
+        return cls(value)
+
+
+def test_cache_output(tmp_path: Path, temp_cache: TrainingCache):
+    model_storage = ModelStorage(tmp_path)
+
+    fingerprint_key = uuid.uuid4().hex
+    output = TestCacheableOutput({"something to cache": "dasdaasda"})
+    output_fingerprint = uuid.uuid4().hex
+
+    temp_cache.cache_output(fingerprint_key, output, output_fingerprint, model_storage)
+
+    assert (
+        temp_cache.get_cached_output_fingerprint(fingerprint_key) == output_fingerprint
+    )
+
+    cached_output, output_type = temp_cache.get_cached_result(output_fingerprint)
+
+    assert output_type == TestCacheableOutput
+    assert output_type.from_cache("xy", cached_output, model_storage) == output
+
+
+def test_get_cached_result_with_miss(tmp_path: Path, temp_cache: TrainingCache):
+    model_storage = ModelStorage(tmp_path)
+
+    # Cache something
+    temp_cache.cache_output(uuid.uuid4().hex, None, uuid.uuid4().hex, model_storage)
+
+    assert temp_cache.get_cached_result(uuid.uuid4().hex) == (None, None)
+    assert temp_cache.get_cached_output_fingerprint(uuid.uuid4().hex) is None
+
+
+def test_get_cached_result_when_result_no_longer_available(
+    tmp_path: Path, monkeypatch: MonkeyPatch
+):
+    model_storage = ModelStorage(tmp_path)
+
+    monkeypatch.setenv(CACHE_LOCATION_ENV, str(tmp_path))
+
     cache = TrainingCache()
 
-    fingerprint_key = "1234"
-    output = 1234
+    output = TestCacheableOutput({"something to cache": "dasdaasda"})
+    output_fingerprint = uuid.uuid4().hex
 
-    cache.cache_output(fingerprint_key, output, output_fingerprint, model_storage)
+    cache.cache_output(uuid.uuid4().hex, output, output_fingerprint, model_storage)
+
+    # Pretend something deleted the cache in between
+    for path in tmp_path.glob("*"):
+        if path.is_dir():
+            shutil.rmtree(path)
+
+    assert cache.get_cached_result(output_fingerprint) == (None, None)
+
+
+def test_cache_creates_location_if_missing(tmp_path: Path, monkeypatch: MonkeyPatch):
+    cache_location = tmp_path / "directory does not exist yet"
+
+    monkeypatch.setenv(CACHE_LOCATION_ENV, str(cache_location))
+
+    _ = TrainingCache()
+
+    assert cache_location.is_dir()
+
+
+def test_caching_something_which_is_not_cacheable(
+    tmp_path: Path, temp_cache: TrainingCache
+):
+    model_storage = ModelStorage(tmp_path)
+
+    # Cache something
+    fingerprint_key = uuid.uuid4().hex
+    output_fingerprint_key = uuid.uuid4().hex
+    temp_cache.cache_output(
+        fingerprint_key, None, output_fingerprint_key, model_storage
+    )
+
+    # Output fingerprint was saved
+    assert (
+        temp_cache.get_cached_output_fingerprint(fingerprint_key)
+        == output_fingerprint_key
+    )
+
+    # But it's not stored to disk
+    assert temp_cache.get_cached_result(output_fingerprint_key) == (None, None)
+
+
+def test_caching_cacheable_fails(
+    tmp_path: Path, caplog: LogCaptureFixture, temp_cache: TrainingCache
+):
+    model_storage = ModelStorage(tmp_path)
+
+    fingerprint_key = uuid.uuid4().hex
+
+    # `tmp_path` is not a dict and will hence fail to be cached
+    # noinspection PyTypeChecker
+    output = TestCacheableOutput(tmp_path)
+    output_fingerprint = uuid.uuid4().hex
+
+    with caplog.at_level(logging.ERROR):
+        temp_cache.cache_output(
+            fingerprint_key, output, output_fingerprint, model_storage
+        )
+
+    assert len(caplog.records) == 1
+
+    assert (
+        temp_cache.get_cached_output_fingerprint(fingerprint_key) == output_fingerprint
+    )
+
+    assert temp_cache.get_cached_result(output_fingerprint) == (None, None)
